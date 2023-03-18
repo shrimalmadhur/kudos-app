3fecf8540a3348ef2cde19d2e0b7cc95766ced64
Madhur Shrimal <madhur.shrimal@gmail.com>
2023-03-17 23:19:52 -0700

    update UI and minting

--- src/components/ListAllNFT.tsx
+++ src/components/ListAllNFT.tsx
@@ -2,7 +2,7 @@ import { FC, useContext, useEffect, useRef, useState } from 'react';
 import axios from "axios";
 import React from 'react'
 import { ethers } from 'ethers'
-import { Network, Alchemy, NftTokenType } from 'alchemy-sdk'
+import { Network, Alchemy, NftTokenType, Wallet } from 'alchemy-sdk'
 import { TokenResponse } from '../../types';
 import { WalletState } from '@web3-onboard/core';
 import erc721abi from '../../artifacts/erc721-abi.json'
@@ -18,6 +18,7 @@ const chainType = process.env.NEXT_PUBLIC_CHAIN
 
 const ListAllNFT: FC<Props> = (data) => {
 
+
   // const address = data.wallet.accounts[0].address;
 
   // const chainId = data.wallet.chains[0].id;
@@ -27,34 +28,6 @@ const ListAllNFT: FC<Props> = (data) => {
   // }
   const defaultAtta: TokenResponse[] = []
   const [nftData, setNftData] = useState(defaultAtta)
-
-  // const burnToken = async function (token: TokenResponse) {
-  //   // create an ethers provider
-  //   let ethersProvider
-  //   ethersProvider = new ethers.providers.Web3Provider(data.wallet.provider, 'any');
-
-  //   const signer = ethersProvider.getSigner(data.wallet.accounts[0].address);
-
-  //   // explore https://github.com/alephao/erc721-batch-transfer for batch transfer
-  //   try {
-  //     let nftContract;
-  //     if (token.tokenType === NftTokenType.ERC721) {
-  //       nftContract = new ethers.Contract(token.contractAddress, erc721abi, signer);
-  //       const tx = await nftContract["safeTransferFrom(address,address,uint256)"](address, "0x000000000000000000000000000000000000dead", token.tokenId, { gasPrice: 20e9 })
-  //       const receipt = await tx.wait()
-  //     } else if (token.tokenType === NftTokenType.ERC1155) {
-  //       nftContract = new ethers.Contract(token.contractAddress, erc1155abi, signer);
-  //       const tx = await nftContract["safeTransferFrom(address,address,uint256,uint256,bytes)"](address, "0x000000000000000000000000000000000000dead", token.tokenId, 1, ethers.constants.HashZero, { gasPrice: 20e9 })
-  //       const receipt = await tx.wait()
-  //     } else {
-  //       console.log("invalid token type: ", token.tokenType)
-  //       return;
-  //     }
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
   useEffect(() => {
     axios.get('/api/v1/' + chainType + '/nft/contract/' + contractAddress)
       .then(response => {
@@ -68,10 +41,10 @@ const ListAllNFT: FC<Props> = (data) => {
     <div className='w-full'>
       {
         nftData
-          ? <div className='w-full'>{nftData.map(element => {
+          ? <div className='grid grid-cols-4 gap-4'>{nftData.map(element => {
             return (
               <div className='w-full space-between md:h-auto flex flex-col md:flex-row rounded-full'>
-                <NftCard token={element}></NftCard>
+                <NftCard token={element} wallet={data.wallet}></NftCard>
               </div>
             )
           })}</div>
--- src/components/NftCard.tsx
+++ src/components/NftCard.tsx
@@ -1,29 +1,63 @@
 import { FC, useContext, useEffect, useRef, useState } from 'react';
 import axios from "axios";
 import React from 'react'
-import { ethers } from 'ethers'
+import { ethers, JsonRpcSigner } from 'ethers'
 import { Network, Alchemy, NftTokenType } from 'alchemy-sdk'
 import { TokenResponse } from '../../types';
 import { WalletState } from '@web3-onboard/core';
 import erc721abi from '../../artifacts/erc721-abi.json'
-import erc1155abi from '../../artifacts/erc1155-abi.json'
+import erc1155abi from '../../artifacts/nft-contract.json'
+import { useConnectWallet } from '@web3-onboard/react';
 
 type Props = {
   token: TokenResponse
+  wallet?: WalletState
 }
 
+const contractAddress = process.env.NEXT_PUBLIC_KUDOS_CONTRACT || ""
+const chainType = process.env.NEXT_PUBLIC_CHAIN || ""
+
 const NftCard: FC<Props> = (data) => {
+  const [showModal, setShowModal] = useState(false);
+  const [mintAddress, setMintAddress] = useState("");
+  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
 
   let img = data.token.rawImageUrl?.split("://")
-  const finalImg = "https://ipfs.io/ipfs/" + img[1];
+  const finalImg = "https://gateway.pinata.cloud/ipfs/" + img[1];
   const divStyle = {
     backgroundImage: 'url(' + finalImg + ')'
   }
+
+  const mintTo = async function() {
+
+    if (!wallet) {
+      return
+    }
+
+    let signer = null;
+    let provider;
+    if (window.ethereum == null) {
+        console.log("MetaMask not installed; using read-only defaults")
+        provider = ethers.getDefaultProvider(chainType)
+    } else {
+        provider = new ethers.BrowserProvider(window.ethereum)
+        signer = await provider.getSigner();
+    }
+
+    try {
+      let nftContract = new ethers.Contract(contractAddress, erc1155abi, signer);
+      const tx = await nftContract["mint(address,uint256)"](mintAddress, data.token.tokenId, { gasLimit: 100000, value: ethers.parseEther('0.005') })
+      const receipt = await tx.wait();
+      console.log(receipt)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div>
       <div className="w-80 bg-white shadow rounded">
-        <div className="h-48 w-full bg-gray-200 flex flex-col justify-between p-4 bg-cover bg-center" style={divStyle}>
-
+        <div className="h-64 w-full bg-gray-200 flex flex-col justify-between p-4 bg-cover bg-center" style={divStyle}>
         </div>
         <div className="p-4 flex flex-col items-center">
           <h1 className="text-gray-800 text-center mt-1">{data.token.name}</h1>
@@ -41,11 +75,74 @@ const NftCard: FC<Props> = (data) => {
               </svg>
             </button>
           </div> */}
-          <button className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 active:bg-blue-700 disabled:opacity-50 mt-4 w-full flex items-center justify-center">
+          <button 
+            className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 active:bg-blue-700 disabled:opacity-50 mt-4 w-full flex items-center justify-center" 
+            onClick={() => { wallet ? setShowModal(true) : connect()}}
+          >
             Mint To
           </button>
         </div> 
       </div> 
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-green-500 text-3xl font-semibold">
+                    {data.token.name}
+                  </h3>
+                  <button
+                    className="text-red-500 p-1 ml-auto bg-transparent border-0 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="text-red-500 bg-transparent h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      x
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <label className="flex flex-col-reverse relative focus group">
+      
+                    <input type="text" name="address" required
+                        className="border-2 border-black px-4 py-3 leading-9" size={64} onChange={(e) => setMintAddress(e.target.value)}/>
+                    
+                    <span className="text-green-500 absolute text-xl transform -translate-y-3 left-4 transition leading-10 group-focus-within:-translate-y-16">
+                      ENS or Address
+                    </span>
+
+                    <span className="text-red-500 ml-auto leading-10">* Required</span>
+                  
+                  </label>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 hover:text-red-900 hover:bg-green"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => mintTo()}
+                  >
+                    Mint
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </div>
   )
 }
